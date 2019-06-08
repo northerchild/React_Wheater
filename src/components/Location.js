2139c4669b406a15ea7b9ebb1576bd637a901844
northerchild <osarus-13@hotmail.com>
2019-06-08 15:36:39 -0500

    Se crean los dos componentes

---
+++ src/components/Location.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Location = ()=>(
+    <div><h1>Bogotá</h1></div>
+    )
+
+export default Location;    
+    
---
+++ src/components/WheaterLocation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Location from './Location';
+import WeatherData from './WeatherData';
+const WheaterLocation = () =>(
+    <div> 
+        <Location></Location>
+        <WeatherData></WeatherData>
+    </div>
+);
+
+export default WheaterLocation;
