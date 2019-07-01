d045a4be3dbadfa9501373380656fd82fdb4b62a
northerchild <osarus-13@hotmail.com>
2019-07-01 14:36:12 -0500

    Se agregan las constantes

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import WeatherExtraInfo from './WeatherExtraInfo';
 import WeatherTemperature from './WeatherTemperature';
+import {
+    CLOUD,
+    CLOUDY,
+    SUN,
+    RAIN,
+    SNOW,
+    WINDY,
+} from './../constants/weathers';
+
 const WeatherData = () => ( <
     div >
     <
     WeatherExtraInfo humidity = { 80 }
     wind = { "10 m/s" } > < /WeatherExtraInfo> <
-    WeatherTemperature temperature = { 20 } > < /WeatherTemperature> < /
+    WeatherTemperature temperature = { 20 }
+    weatherState = { CLOUDY } >
+    <
+    /WeatherTemperature> < /
     div >
 )
 
